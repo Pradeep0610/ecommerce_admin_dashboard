b17d651cbfdd043d03ac96083da0f2cb8e7c3ae5
Pradeep Subedi <cenapradeep94@gmail.com>
2020-09-02 17:46:21 +0545

    deploy

---
+++ precache-manifest.81558e7e020208ab633e573d7ee87cab.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b83937487a2830d2d3610227587e507",
+    "url": "/ecommerce_admin_dashboard/assets/photo-1598022926764"
+  },
+  {
+    "revision": "a26aa5e4fa31175a951d",
+    "url": "/ecommerce_admin_dashboard/css/app.823086b6.css"
+  },
+  {
+    "revision": "0ad55fb0c8f2a41536e0",
+    "url": "/ecommerce_admin_dashboard/css/chunk-0b84dd00.60c410bd.css"
+  },
+  {
+    "revision": "b9e3808755d59fa8a524",
+    "url": "/ecommerce_admin_dashboard/css/chunk-12a294ff.3b53fc44.css"
+  },
+  {
+    "revision": "1e8fd845dcfd9000a5b4",
+    "url": "/ecommerce_admin_dashboard/css/chunk-13685546.201e65ec.css"
+  },
+  {
+    "revision": "e644dc4c8611f13095cf",
+    "url": "/ecommerce_admin_dashboard/css/chunk-21a8e636.ae75465a.css"
+  },
+  {
+    "revision": "eccbf63185fe483c2f37",
+    "url": "/ecommerce_admin_dashboard/css/chunk-4e325580.17ad18d3.css"
+  },
+  {
+    "revision": "dd817046cc52a90c7dd7",
+    "url": "/ecommerce_admin_dashboard/css/chunk-f337c8cc.9c060e45.css"
+  },
+  {
+    "revision": "96a19a64c0f0f166e99a",
+    "url": "/ecommerce_admin_dashboard/css/chunk-vendors.61893cbb.css"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/ecommerce_admin_dashboard/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/ecommerce_admin_dashboard/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/ecommerce_admin_dashboard/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "/ecommerce_admin_dashboard/fonts/MaterialIcons-Regular.d120c85b.ttf"
+  },
+  {
+    "revision": "3d2fa2e544004aef2ea641698b715af6",
+    "url": "/ecommerce_admin_dashboard/fonts/feather.3d2fa2e5.woff"
+  },
+  {
+    "revision": "6a750f0119ba9c8ab8a994a4796599a6",
+    "url": "/ecommerce_admin_dashboard/fonts/feather.6a750f01.ttf"
+  },
+  {
+    "revision": "931c9e74d0b20947054bfe6a5b74a838",
+    "url": "/ecommerce_admin_dashboard/fonts/feather.931c9e74.eot"
+  },
+  {
+    "revision": "8edbcdedecdd3284f9aaa19b417c8b5a",
+    "url": "/ecommerce_admin_dashboard/img/avatar-s-1.8edbcded.png"
+  },
+  {
+    "revision": "7be10c5705a8bd9eb9bc9bca9ff30da6",
+    "url": "/ecommerce_admin_dashboard/img/avatar-s-10.7be10c57.png"
+  },
+  {
+    "revision": "51a23c074bd7469a69e220c62dce541a",
+    "url": "/ecommerce_admin_dashboard/img/avatar-s-11.51a23c07.png"
+  },
+  {
+    "revision": "cec148feffe8d6a77c51ab10fc3013d0",
+    "url": "/ecommerce_admin_dashboard/img/avatar-s-12.cec148fe.png"
+  },
+  {
+    "revision": "1875411a1a1c12898f9cd4ef98996d45",
+    "url": "/ecommerce_admin_dashboard/img/avatar-s-13.1875411a.png"
+  },
+  {
+    "revision": "9ab8c5145b6b0ca0f39eea73aabead5f",
+    "url": "/ecommerce_admin_dashboard/img/avatar-s-14.9ab8c514.png"
+  },
+  {
+    "revision": "30fed846366adf11f8ee1014fb2185c9",
+    "url": "/ecommerce_admin_dashboard/img/avatar-s-15.30fed846.png"
+  },
+  {
+    "revision": "88e40556adbbc9ce12c1d79bd2a06531",
+    "url": "/ecommerce_admin_dashboard/img/avatar-s-16.88e40556.png"
+  },
+  {
+    "revision": "a92e96b20acdf06888462dd9ef51e5a1",
+    "url": "/ecommerce_admin_dashboard/img/avatar-s-17.a92e96b2.png"
+  },
+  {
+    "revision": "6f5c3253da8a82c2cb14bae61d84a2dd",
+    "url": "/ecommerce_admin_dashboard/img/avatar-s-18.6f5c3253.png"
+  },
+  {
+    "revision": "908c8b7896636515e5ec2e874c067e4b",
+    "url": "/ecommerce_admin_dashboard/img/avatar-s-19.908c8b78.png"
+  },
+  {
+    "revision": "93c7a17a77d076a725b4280c8d45fcc8",
+    "url": "/ecommerce_admin_dashboard/img/avatar-s-2.93c7a17a.png"
+  },
+  {
+    "revision": "aa019f1dca634669abf1ccbb56167412",
+    "url": "/ecommerce_admin_dashboard/img/avatar-s-20.aa019f1d.png"
+  },
+  {
+    "revision": "5aee13d88923688c3e2766cee073e823",
+    "url": "/ecommerce_admin_dashboard/img/avatar-s-21.5aee13d8.png"
+  },
+  {
+    "revision": "350e5390e5333dd0a18dce3744d7b860",
+    "url": "/ecommerce_admin_dashboard/img/avatar-s-22.350e5390.png"
+  },
+  {
+    "revision": "5e55f9497dfc1b8fac16cc142c59f189",
+    "url": "/ecommerce_admin_dashboard/img/avatar-s-23.5e55f949.png"
+  },
+  {
+    "revision": "13572d01d6c52d379b94d8ee42f19c14",
+    "url": "/ecommerce_admin_dashboard/img/avatar-s-24.13572d01.png"
+  },
+  {
+    "revision": "d1c0b48d54f68a0e3dc22dd3cabac6ef",
+    "url": "/ecommerce_admin_dashboard/img/avatar-s-25.d1c0b48d.png"
+  },
+  {
+    "revision": "d1c0b48d54f68a0e3dc22dd3cabac6ef",
+    "url": "/ecommerce_admin_dashboard/img/avatar-s-26.d1c0b48d.png"
+  },
+  {
+    "revision": "a718f09ae7f1fb4a74bbc5d919ffb041",
+    "url": "/ecommerce_admin_dashboard/img/avatar-s-3.a718f09a.png"
+  },
+  {
+    "revision": "88863dbbc2ac97ccf75be4ace8c8e9fe",
+    "url": "/ecommerce_admin_dashboard/img/avatar-s-4.88863dbb.png"
+  },
+  {
+    "revision": "73b9c8f89f23bd02f99ba036a70b5562",
+    "url": "/ecommerce_admin_dashboard/img/avatar-s-5.73b9c8f8.png"
+  },
+  {
+    "revision": "6b63817cd7446ee6b140a297e22dd3be",
+    "url": "/ecommerce_admin_dashboard/img/avatar-s-6.6b63817c.png"
+  },
+  {
+    "revision": "9241b5d6708f97ada7c9cfeaa15a052d",
+    "url": "/ecommerce_admin_dashboard/img/avatar-s-7.9241b5d6.png"
+  },
+  {
+    "revision": "2579bd6d8eef35e22e7ec84b69bcee3b",
+    "url": "/ecommerce_admin_dashboard/img/avatar-s-8.2579bd6d.png"
+  },
+  {
+    "revision": "ec990ac17b3841dd6b9c4dd5cb96be6b",
+    "url": "/ecommerce_admin_dashboard/img/avatar-s-9.ec990ac1.png"
+  },
+  {
+    "revision": "5f6aa47cad6e07be32f187d78f4f1d1c",
+    "url": "/ecommerce_admin_dashboard/img/bank.5f6aa47c.png"
+  },
+  {
+    "revision": "0e5daf6bac4f17e400266545070b108e",
+    "url": "/ecommerce_admin_dashboard/img/feather.0e5daf6b.svg"
+  },
+  {
+    "revision": "a57a277a5fbd8b477ddf5d2ef1efa3e4",
+    "url": "/ecommerce_admin_dashboard/index.html"
+  },
+  {
+    "revision": "a26aa5e4fa31175a951d",
+    "url": "/ecommerce_admin_dashboard/js/app.b462430b.js"
+  },
+  {
+    "revision": "0ad55fb0c8f2a41536e0",
+    "url": "/ecommerce_admin_dashboard/js/chunk-0b84dd00.e3bac373.js"
+  },
+  {
+    "revision": "b9e3808755d59fa8a524",
+    "url": "/ecommerce_admin_dashboard/js/chunk-12a294ff.303e2604.js"
+  },
+  {
+    "revision": "1e8fd845dcfd9000a5b4",
+    "url": "/ecommerce_admin_dashboard/js/chunk-13685546.752c67d7.js"
+  },
+  {
+    "revision": "e644dc4c8611f13095cf",
+    "url": "/ecommerce_admin_dashboard/js/chunk-21a8e636.d9d5416d.js"
+  },
+  {
+    "revision": "eccbf63185fe483c2f37",
+    "url": "/ecommerce_admin_dashboard/js/chunk-4e325580.70745569.js"
+  },
+  {
+    "revision": "dd817046cc52a90c7dd7",
+    "url": "/ecommerce_admin_dashboard/js/chunk-f337c8cc.0bfbce99.js"
+  },
+  {
+    "revision": "96a19a64c0f0f166e99a",
+    "url": "/ecommerce_admin_dashboard/js/chunk-vendors.b2ca161a.js"
+  },
+  {
+    "revision": "ad1c7122216ff6e919bd68f90e80d94d",
+    "url": "/ecommerce_admin_dashboard/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ecommerce_admin_dashboard/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ecommerce_admin_dashboard/precache-manifest.81558e7e020208ab633e573d7ee87cab.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "admin"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
